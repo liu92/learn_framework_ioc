584c59344a3c0d3907cdc4f97e4276b027bf5010
liu9209 <470437289@qq.com>
2019-08-06 00:11:39 +0800

    update:自己写一个简单的ioc

---
+++ src/main/java/core/BeanFactory.java
@@ -0,0 +1,13 @@
+package core;
+
+/**
+ * Bean 工厂
+ * @ClassName: BeanFactory
+ * @Description: 根据springioc原理写一个简单IOC 框架
+ * @Author: lin
+ * @Date: 2019/8/5 23:48
+ * @History:
+ * @<version> 1.0
+ */
+public interface BeanFactory {
+}
